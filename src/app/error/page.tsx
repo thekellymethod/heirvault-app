1fd0e8005cf583e6c2b86e57da962a89612a7425
thekellymethod <thekellymethod@outlook.com>
2025-12-31 15:24:22 -0600

    refactor: remove unused Button import in error page
    - Commented out the import statement for the Button component in the error page to clean up the code and reduce unnecessary dependencies.
    - This change helps improve code clarity and maintainability by removing unused code elements.

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { Logo } from "@/components/Logo";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import {
   Shield,
   XCircle,
